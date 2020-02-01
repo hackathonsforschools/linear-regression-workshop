ff2256886dcb699216aec58c8d69e822f9ef55f0
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-02-01 19:11:57 +0000

    Basic linear regression example

---
+++ linear-regression.py
@@ -0,0 +1,23 @@
+xVals = [-100, -10, -3, 0, 1, 2, 3, 4 , 10, 100]
+yVals = [9901, 91, 7, 1, 3,7, 13, 21, 111, 10101]
+
+iterations = 100000
+
+w0 = 0
+w1 = 0
+w2 = 0
+
+alpha = 0.00000001
+
+for i in range(0, iterations):
+    cost = 0
+
+    for j in range(0, len(xVals)):
+        prediction = w0 + (w1 * xVals[j]) + (w2 * xVals[j] * xVals[j])
+        difference = yVals[j] - prediction
+
+        w0 += alpha * difference
+        w1 += alpha * difference * xVals[j]
+        w2 += alpha * difference * xVals[j] * xVals[j]
+
+print("Equation: hw(x) = " + str(w2) +"*x^2 + " + str(w1) + "*x + " + str(w0))
